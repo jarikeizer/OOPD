010b4d11bb95b04e650c7edfc6bcabaeeacd1c64
Jari Keizer <jarikeizer16@gmail.com>
2018-03-02 14:49:40 +0100

    verandering in studieadvies project

--- Week 3 les 3/Studieadvies/src/main/Studieadviseur.java
+++ Week 3 les 3/Studieadvies/src/main/Studieadviseur.java
@@ -4,24 +4,14 @@ public class Studieadviseur {
 	public static boolean krijgtPositiefStudieadvies(Student s) {
 		int aantalcijfershogerdan6 = 0;
 		int[] cijfers = s.getCijfers();
-		for(int i = 0; i < 8; i++)
-		{
-			if(cijfers[i] > 6)
-			{
+		for (int i = 0; i < cijfers.length; i++) {
+			if (cijfers[i] > 6) {
 				aantalcijfershogerdan6++;
 			}
 		}
-		
-		
-		if(aantalcijfershogerdan6 >= 4)
-		{
-			return true;
-		}
-		else {
-			return false;
-		}
-		
-		
+		return aantalcijfershogerdan6 >= 4;
+	
+
 	}
 
 }
